0214bcb3284bfe2bf35ac8dbf9da0e6353d6a2a5
Tintu Thomas <tintu.thomas@arm.com>
2024-09-18 14:55:06 +0100

    tc: Split bootloader config as common and platform specific
    Signed-off-by: Tintu Thomas <tintu.thomas@arm.com>

--- product/totalcompute/tc3/scp_boot/config_bootloader.c
+++ product/totalcompute/tc3/scp_boot/config_bootloader.c
@@ -5,20 +5,9 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include "scp_mmap.h"
-#include "tc_sds.h"
+#include <config_common_bootloader.h>
 
-#include <mod_bootloader.h>
-
-#include <fwk_module.h>
-
-static const struct mod_bootloader_config bootloader_module_config = {
-    .source_base = SCP_TRUSTED_RAM_BASE,
-    .source_size = 512 * 1024,
-    .destination_base = SCP_RUNTIME_BASE,
-    .destination_size = SCP_RUNTIME_SIZE,
-    .sds_struct_id = TC_SDS_BOOTLOADER,
-};
+GENERATE_BOOTLOADER_CFG(SCP_RUNTIME_BASE, SCP_RUNTIME_SIZE)
 
 struct fwk_module_config config_bootloader = {
     .data = &bootloader_module_config,
